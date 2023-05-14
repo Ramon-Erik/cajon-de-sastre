06f27aaf0aa998753bc941424ce57fd0e5e8c9fc
Ramon Erik <ramonerik29a@gmail.com>
2023-05-14 20:53:34 -0300

    implementando as funções menu

--- js/script.js
+++ js/script.js
@@ -1,35 +1,32 @@
-// class NavBar {
-//     constructor(menu, listaNav, linksNav) {
-//         this.menu = document.querySelector(menu)
-//         this.listaNav = document.querySelector(listaNav)
-//         this.linksNav = document.querySelectorAll(linksNav)
-//         this.activeClass = 'Active'
-//     }
+class NavBar {
+    constructor(menu, listaNav, linksNav) {
+        this.menu = document.querySelector(menu)
+        this.listaNav = document.querySelector(listaNav)
+        this.linksNav = document.querySelectorAll(linksNav)
+        this.activeClass = 'Active'
+        this.handleClick = this.handleClick.bind()
+    }
 
-//     addClickEvent() {
-//         this.menu.addEventListener('click', () => console.log('hey 🛴'))
-//     }
+    handleClick() {
+        this.listaNav.classList.toggle(this.activeClass)
+    }
 
-//     init() {
-//         console.log('a')
+    addClickEvent() {
+        this.menu.addEventListener('click',this.handleClick())
+    }
 
-//         if (this.menu) {
-//             this.addClickEvent()
-//         }
-//         return this
-//     }
-// }
+    init() {
+        if (this.menu) {
+            this.addClickEvent()
+        }
+        return this
+    }
+}
 
-// const menu = new NavBar(
-//     '.btn-menu',
-//     '.nav-lista',
-//     '.nav-list li'
-// )
+const menu = new NavBar(
+    '.btn-menu',
+    '.nav-lista',
+    '.nav-list li'
+)
 
-// menu.init()
-
-
-// let maenu = document.querySelector('.btn-menu')
-console.log('maenu')
-
-alert('oi')jkkbdb jhasv
+menu.init()
