d9a736855198a4084f283d00df8e9784af0b8a8a
Ramon Erik <ramonerik29a@gmail.com>
2023-05-14 20:34:09 -0300

    funções menu

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// class NavBar {
+//     constructor(menu, listaNav, linksNav) {
+//         this.menu = document.querySelector(menu)
+//         this.listaNav = document.querySelector(listaNav)
+//         this.linksNav = document.querySelectorAll(linksNav)
+//         this.activeClass = 'Active'
+//     }
+
+//     addClickEvent() {
+//         this.menu.addEventListener('click', () => console.log('hey 🛴'))
+//     }
+
+//     init() {
+//         console.log('a')
+
+//         if (this.menu) {
+//             this.addClickEvent()
+//         }
+//         return this
+//     }
+// }
+
+// const menu = new NavBar(
+//     '.btn-menu',
+//     '.nav-lista',
+//     '.nav-list li'
+// )
+
+// menu.init()
+
+
+// let maenu = document.querySelector('.btn-menu')
+console.log('maenu')
+
+alert('oi')jkkbdb jhasv
