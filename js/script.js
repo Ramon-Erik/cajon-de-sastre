dc53f7c280687d905ec3d439950b58d16c3e09cd
Ramon Erik <ramonerik29a@gmail.com>
2023-05-14 21:42:11 -0300

    funções do menu
    dar uma olhada quanto a quando a pagina for aberta por celulares pois o overflowx não funciona

--- js/script.js
+++ js/script.js
@@ -3,16 +3,26 @@ class NavBar {
         this.menu = document.querySelector(menu)
         this.listaNav = document.querySelector(listaNav)
         this.linksNav = document.querySelectorAll(linksNav)
-        this.activeClass = 'Active'
-        this.handleClick = this.handleClick.bind()
+        this.activeClass = 'active'
+        this.handleClick = this.handleClick.bind(this)
+    }
+    
+    animateLinks() {
+        this.linksNav.forEach((link, index) => {
+            link.style.animation 
+            ? (link.style.animation = '')
+            :( link.style.animation = `navLinkFade .5s ease forwards ${index / 7 + .3}s`)
+        })
     }
 
     handleClick() {
         this.listaNav.classList.toggle(this.activeClass)
+        this.menu.classList.toggle(this.activeClass)
+        this.animateLinks()
     }
 
     addClickEvent() {
-        this.menu.addEventListener('click',this.handleClick())
+        this.menu.addEventListener('click', this.handleClick)
     }
 
     init() {
@@ -26,7 +36,7 @@ class NavBar {
 const menu = new NavBar(
     '.btn-menu',
     '.nav-lista',
-    '.nav-list li'
+    '.nav-lista li'
 )
 
 menu.init()
