783712d95d379d361aeb5b45d42ce8229a9c3c2d
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-06-12 14:21:23 -0300

    menu fixo adaptado ao scroll

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@ const menu = document.querySelector('.links-menu')
 const menuBtn = document.querySelector('.btn-menu')
 const fade = document.querySelector('.fade')
 
+function scrollT() {
+    return document.documentElement.scrollTop
+}
+
 function mostrarMenu() {
     if (menu.classList.contains('ativado')) {
         tirarMenu()
     } else {
         menu.classList.add('ativado')
         menuBtn.classList.add('ativado')
+        menuBtn.classList.add('scroll')
         fade.classList.add('ativado')
     }
 }
@@ -15,5 +20,19 @@ function mostrarMenu() {
 function tirarMenu() {
     menu.classList.remove('ativado')
     menuBtn.classList.remove('ativado')
+    if (scrollT() < 113) {
+        menuBtn.classList.remove('scroll')
+    }
     fade.classList.remove('ativado')
 }
+
+document.addEventListener('scroll', () => {
+    const verticalPosition = scrollT()
+    
+    console.log(verticalPosition)
+    if (verticalPosition >= 113) {
+        menuBtn.classList.add('scroll')
+    } else {
+        menuBtn.classList.remove('scroll')
+    }
+})
